b715ea2446b0d28712920a97cc7053ce0272c12f
Lukas <lukas.tatge@cimk.net>
2024-05-30 16:35:06 +0800

    Update v3.5

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -10,7 +10,7 @@ export default function SignUp() {
   const [submitted, setSubmitted] = useState(false);
   const router = useRouter();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     setSubmitted(true);
     setTimeout(() => {
